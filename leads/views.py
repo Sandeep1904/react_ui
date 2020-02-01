c343dbbb33f254e12c41629dee720f98e2588e16
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-01 11:00:08 +0530

    APIView

--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
-def test_view(request):
-    data = {
-        'name' : 'sandeep',
-        'age': 18
-    }
-    return JsonResponse(data)
+
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name' : 'sandeep',
+            'age': 18
+        }
+        return Response(data)
+
+
+# def test_view(request):
+#     data = {
+#         'name' : 'sandeep',
+#         'age': 18
+#     }
+#     return JsonResponse(data)
--- react_ui/urls.py
+++ react_ui/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from leads.views import test_view
+from leads.views import TestView
 urlpatterns = [
-    path('', test_view, name='test'),
+    path('', TestView.as_view(), name='test'),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls)
 ]
