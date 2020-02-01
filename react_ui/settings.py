36f000e6de9b3d00446a42df59de85206abe7d28
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-01 14:17:26 +0530

    new authentication

--- react_ui/settings.py
+++ react_ui/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
+    'rest_auth',
     'rest_framework.authtoken',
     'rest_framework'
 ]
--- react_ui/urls.py
+++ react_ui/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', TestView.as_view(), name='test'),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/token/', obtain_auth_token, name= 'obtain-token')
+    path('api/token/', obtain_auth_token, name= 'obtain-token'),
+    path('rest-auth/', include('rest_auth.urls'))
 ]
